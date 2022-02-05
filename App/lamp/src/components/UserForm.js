8b9ea8f098461f506780cfa1e469327edcad6adc
mingkaic1 <mingkaic@andrew.cmu.edu>
2022-02-05 00:02:40 -0500

    Buggy React

--- App/lamp/src/components/UserForm.js
+++ App/lamp/src/components/UserForm.js
@@ -1,7 +1,34 @@
+import react from "react";
+
+/*
 const UserForm = () => {
     return (
-        <h1>Test</h1>
+        <h1>Hello World</h1>
     );
+}
+*/
+
+export class UserForm extends react.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <div className="UserForm">
+                <h2>Form</h2>
+                <form onSubmit={this.handleSubmit} onReset={this.handleReset}>
+                    <input 
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={this.state.nameValue}
+                        onChange={this.handleNameChange}
+                        placeholder="Username"
+                    />
+                </form>
+            </div>
+        );
+    }
 }
 
 export default UserForm;

1580861610aa06781cf98cefcbf3a836409865a6
mingkaic1 <mingkaic@andrew.cmu.edu>
2022-02-05 00:12:36 -0500

    Starting UserForm

--- App/lamp/src/components/UserForm.js
+++ App/lamp/src/components/UserForm.js
@@ -1,5 +1,7 @@
 import react from "react";
 
+// TO CONVERT ALL INTO ARROW FUNCTIONS
+
 /*
 const UserForm = () => {
     return (
@@ -11,6 +13,29 @@ const UserForm = () => {
 export class UserForm extends react.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            nameValue: "",
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        // Submit data
+        this.setState({
+            nameValue: "",
+        });
+    }
+    handleReset(event) {
+        this.setState({
+            nameValue: "",
+        });
+    }
+    handleNameChange(event) {
+        this.setState({
+            nameValue: event.target.value,
+        });
     }
     render() {
         return(
@@ -25,6 +50,12 @@ export class UserForm extends react.Component {
                         onChange={this.handleNameChange}
                         placeholder="Username"
                     />
+                    <button type="submit">
+                        Submit
+                    </button>
+                    <button type="reset">
+                        Reset
+                    </button>
                 </form>
             </div>
         );
